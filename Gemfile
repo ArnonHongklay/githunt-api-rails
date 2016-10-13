43791dcee696bf3a69d4bfada2a14d767fdf0cff
Tom Coleman <tom@thesnail.org>
2016-10-13 23:35:23 +1100

    Added a etag cache for GH API

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.1.0'
 
 gem 'dotenv-rails', :groups => [:development, :test]
 gem 'octokit'
+gem 'faraday-http-cache'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -2,3 +2,10 @@ Octokit.configure do |c|
   c.client_id = ENV['GITHUB_CLIENT_ID']
   c.client_secret = ENV['GITHUB_CLIENT_SECRET']
 end
+
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache, serializer: Marshal, shared_cache: false
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
+Octokit.middleware = stack
